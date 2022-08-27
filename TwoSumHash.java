e66e3ce26f4d22e2c7136e0976cc20f0c55d0b9f
jceric96 <74382751+jceric96@users.noreply.github.com>
2022-08-27 00:09:08 -0300

    clean up code

--- TwoSumHash.java
+++ TwoSumHash.java
@@ -4,26 +4,26 @@ public class TwoSumHash {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i], i);
-        }
-        int[] result = new int[2];
-        for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
             if (map.containsKey(complement) && map.get(complement) != i) {
                 return new int[] { i, map.get(complement) };
             }
-
+            map.put(nums[i], i);
         }
-        return result;
+        return null;
     }
 
     public static void main(String[] args) throws Exception {
         int[] nums = new int[] { 2, 7, 11, 15 };
         int target = 9;
         TwoSumHash twosum = new TwoSumHash();
-        int[] res = twosum.twoSum(nums, target);
-        for (int i = 0; i < res.length; i++) {
-            System.out.println(res[i]);
+        if (twosum.twoSum(nums, target) != null) {
+            int[] res = twosum.twoSum(nums, target);
+            for (int i = 0; i < res.length; i++) {
+                System.out.println(res[i]);
+            }
+        } else {
+            System.out.println(twosum.twoSum(nums, target));
         }
 
     }
